73f5850f100f2f14ab1191d66129696ee6ce3d93
Revan Ferdinand <revancrozz@gmail.com>
2026-07-01 11:47:51 +0700

    first commit

---
+++ drivecompanion/drivecompanionApp.swift
@@ -0,0 +1,17 @@
+//
+//  drivecompanionApp.swift
+//  drivecompanion
+//
+//  Created by Revan Ferdinand on 01/07/26.
+//
+
+import SwiftUI
+
+@main
+struct drivecompanionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
