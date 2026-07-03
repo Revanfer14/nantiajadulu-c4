7f707548d312c39eb90886a43dcc194b6bf18e82
Michelle Nathania <mnathaniaa14@gmail.com>
2026-07-03 09:44:26 +0700

    Theme addition
    Add theme, color, font, reusable components

---
+++ drivecompanion/Components/CardTransparent.swift
@@ -0,0 +1,42 @@
+//
+//  CardTransparent.swift
+//  drivecompanion
+//
+//  Created by Michelle Nathania on 03/07/26.
+//
+
+import SwiftUI
+
+struct CardTransparent: View {
+    let title: String
+    let iconName: String
+    let description: String
+    
+    init(_ title: String, iconName: String, description: String) {
+        self.title = title
+        self.iconName = iconName
+        self.description = description
+    }
+    
+    var body: some View {
+        HStack (spacing: 12) {
+            Image(systemName: iconName)
+                .font(AppFont.iconSize)
+                .foregroundStyle(AppColor.primary)
+            
+            VStack (alignment: .leading) {
+                Text(title)
+                    .font(AppFont.sectionTitle)
+                    .fontWeight(.bold)
+                    .foregroundStyle(AppColor.textPrimary)
+                Text(description)
+                    .font(AppFont.body)
+                    .foregroundStyle(AppColor.textSecondary)
+            }
+        }
+    }
+}
+
+#Preview {
+    CardTransparent("Title", iconName: "camera.circle", description: "Lorem ipsum dolor sit amet lalala lilili halo mintje")
+}
