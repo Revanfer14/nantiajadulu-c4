1ad1114af2a968ca33d47450e8f084d8ee038c7e
Stephanie Vania Suwardi <stphnivsuwardi09@gmail.com>
2026-07-03 10:09:34 +0700

    Create OnboardingView.swift

---
+++ drivecompanion/Views/OnboardingView.swift
@@ -0,0 +1,20 @@
+//
+//  Onboarding.swift
+//  drivecompanion
+//
+//  Created by Stephanie Vania Suwardi Data on 03/07/26.
+//
+
+import SwiftUI
+
+struct OnboardingView: View {
+    var body: some View {
+        Text("Hello, World!")
+        
+        
+    }
+}
+
+#Preview {
+    OnboardingView()
+}
