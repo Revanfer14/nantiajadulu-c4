e2a3b13edee27a3123019c26cb65d4349cca8029
Michelle Nathania <mnathaniaa14@gmail.com>
2026-07-06 21:07:46 +0700

    Split DrowsinessState
    Split file into Shared, iPhone, and Watch (specific interactions & UI using enums)

---
+++ SharedDrowsinessState.swift
@@ -0,0 +1,16 @@
+//
+//  DrowsinessState.swift
+//  drivecompanion
+//
+//  Created by Filbert Naldo Wijaya on 02/07/26.
+//
+
+// MARK: Initial enum
+enum DrowsinessState: String {
+    case alert
+    case drowsy
+    case microsleep
+    case noFace
+}
+
+// TODO: Add iPhone face using extension to DrowsinessState
