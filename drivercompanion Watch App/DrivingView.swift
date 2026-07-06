2a682bdd4e4ee2e4d5a0dfe60e8e5f84e692ce46
Michelle Nathania <mnathaniaa14@gmail.com>
2026-07-06 21:06:54 +0700

    Rename ContentView to DrivingView

---
+++ drivercompanion Watch App/DrivingView.swift
@@ -0,0 +1,30 @@
+//
+//  DrivingView.swift
+//  drivercompanion Watch App
+//
+//  Created by Michelle Nathania on 06/07/26.
+//
+
+import SwiftUI
+
+struct DrivingView: View {
+    @StateObject private var connectivity = WatchConnectivityManager.shared
+    
+    var body: some View {
+        VStack (spacing: 20) {
+            
+            VStack (spacing: 20) {
+                Image(connectivity.state.watchMascotImage)
+                    .resizable()
+                    .scaledToFit()
+                
+                Text(connectivity.state.watchMascotText)
+            }
+            .padding()
+        }
+    }
+}
+    
+//#Preview {
+//    DrivingView(state: connectivity.state)
+//}
